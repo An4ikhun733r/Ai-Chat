bd6de3fc58d3a1cf8115af5511f758a5600fa29a
An4ikhun733r <anuar.ospanov.prof@gmail.com>
2024-05-20 23:32:26 +0500

    added url for deployment

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -8,6 +8,7 @@ config();
 const app = express();
 //middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ai-chat-one-phi.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 //remove it in production
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,8 @@ const app = express();
 
 //middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ai-chat-one-phi.vercel.app", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
